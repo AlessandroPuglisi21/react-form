3650189c339420ede6368cfe5997ba5c49cd3e5c
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-12-02 15:28:12 +0100

    ADD: New Post

--- components/card.jsx
+++ components/card.jsx
@@ -1,18 +1,46 @@
+import { useState } from "react";
 import style from "../components/card.module.css";
-import posts from "../components/posts";
+import initialPosts from "../components/posts";
 
 export default function Card() {
+  const [title, setTitle] = useState('');
+  const [posts, setPosts] = useState(initialPosts);
+
   const publishedPosts = posts.filter((post) => post.published);
+
+  function addPost(e) {
+    e.preventDefault();
+
+    const newTitle = title.trim();
+    if (newTitle === '') return;
+
+    const addedPost = {
+      title: newTitle,
+      image: 'https://picsum.photos/200/300?grayscale',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.',
+      tags: [],
+      published: true,
+    };
+
+    setPosts([...posts, addedPost]);
+    setTitle('');
+    console.log('Post aggiunto!');
+  }
 
   return (
     <>
       <main className={style.background}>
-
         {/* FORM */}
         <div className={style.formContainer}>
-          <form action="">
-            <input type="text" placeholder="Inserisci il titolo del post" className={style.newPost}/>
-            <input type="submit" className={style.submitButton} />
+          <form onSubmit={addPost} action="">
+            <input
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              placeholder="Inserisci il titolo del post"
+              className={style.newPost}
+            />
+            <input type="submit" value="Aggiungi" className={style.submitButton} />
           </form>
         </div>
 
@@ -21,21 +49,17 @@ export default function Card() {
           {publishedPosts.length > 0
             ? publishedPosts.map((post) => (
                 <div key={post.id} className={style.cardbody}>
-                  <img src={post.image} alt="" />
+                  <img src={post.image || '/path-to-default-image.jpg'} alt={post.title || 'Post'} />
                   <h3>
                     {post.title} {post.id}
                   </h3>
-                  <h5
-                    style={{
-                      color: post.tags.includes("html") ? "red" : "blue",
-                    }}
-                  >
+                  <h5 style={{ color: post.tags.includes("html") ? "red" : "blue" }}>
                     Tag: {post.tags.join("-")}
                   </h5>
                   <p>Contenuto: {post.content}</p>
                 </div>
               ))
-            : ""}
+            : <p>Nessun post pubblicato.</p>}
         </div>
       </main>
     </>
